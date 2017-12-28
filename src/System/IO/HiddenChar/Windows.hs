8003283358d7c95f2c24a43e13b4589a4c27af50
cronokirby <cronokirby@gmail.com>
2017-12-28 16:00:36 +0100

    Fixes deprecated getch
    fixes stuff for windows

--- src/System/IO/HiddenChar/Windows.hs
+++ src/System/IO/HiddenChar/Windows.hs
@@ -17,7 +17,7 @@ import           Foreign.C.Types
 
 -- Must import as "safe" in order to prevent FFI call from blocking other threads
 -- https://github.com/rcook/hidden-char/issues/1
-foreign import ccall safe "conio.h getch" c_getch :: IO CInt
+foreign import ccall safe "conio.h _getch" c_getch :: IO CInt
 
 -- | Read a character from the standard input device with buffering and echoing disabled
 -- Hack based on http://stackoverflow.com/questions/2983974/haskell-read-input-character-from-console-immediately-not-after-newline
